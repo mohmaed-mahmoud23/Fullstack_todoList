a49f1ae466769e4a3e97533f0c156398321b72c2
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-11-21 17:17:19 +0000

    change

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  <div className=" flex items-center justify-center ">
+  <div className=" flex items-center justify-center bg-red-400">
     <SignUp />
   </div>
 }
