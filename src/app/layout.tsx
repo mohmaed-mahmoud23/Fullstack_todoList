73bcfb2ee346d9d79c65a0b3627f575df2e3c82a
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-11-21 18:05:38 +0000

    Remov Handler error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+    <ClerkProvider>
       <html lang="en">
         <body
           className={`${inter.className} bg-white text-black dark:text-white dark:bg-black`}
